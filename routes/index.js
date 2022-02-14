ea5b6e68fa2362d4587860e7b1177e1d7fdc9476
Gianluca Francescato <gianluca@Gianlucas-MacBook-Pro.local>
2022-02-14 19:26:46 +0100

    log in monday

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,25 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+function loginCheck() {
+  return (req, res, next) => {
+    // check if we have a logged in user
+    if (req.user) {
+      // the user making the request is logged in
+      // user can proceed
+      next()
+    } else {
+      // user is not logged in
+      res.redirect('/login')
+    }
+  }
+}
+
+router.get("/profile", loginCheck(), (req, res, next) => {
+  // in node basic auth: req.session.user
+  // in passport: req.user
+  const user = req.user
+  res.render("profile", { user: user });
+});
+
 module.exports = router;
