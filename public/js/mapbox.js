b9048422369acdba6ef6a1f087ff7a0f3d609f94
Mehmet Buyukkose <phunnycy@gmail.com>
2022-02-16 12:16:20 +0100

    connect to the DB

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -15,13 +15,21 @@ const popup = new mapboxgl.Popup({
     closeButton: true
 });
 
-popup.setLngLat([13.404954, 52.560090])
-.setHTML('<h5>Hello Mapbox</h5>')
-.addTo(map);
+let markers = [];
+let coordinates = [];
+let marker;
+let street;
+let city;
+let plz;
+
+// popup.setLngLat([13.404954, 52.560090])
+// .setHTML('<h5>Hello Mapbox</h5>')
+// .addTo(map);
 
 // create a new marker
 map.on('click', addMarker);
 
+// exapmle coords for test purposes - TO BE DELETED
 const coords = [
     [13.405, 52.52],
     [13.6, 52.6]
@@ -48,17 +56,94 @@ function addMarkerFromDB(coord) {
 
 
 // function to add marker on click
+// function addMarker(event) {
+//     const coords = event.lngLat;
+//     console.log(coords);
+//     // set an existing marker on the map
+//     new mapboxgl.Marker({
+//     color: 'red',
+//     draggable: true
+// }).setLngLat(coords)
+// .addTo(map)
+// .on('dragend', event => console.log(event.target._lngLat))
+// };
+
+
 function addMarker(event) {
-    const coords = event.lngLat;
-    console.log(coords);
-    // set an existing marker on the map
-    new mapboxgl.Marker({
+    coordinates = event.lngLat;
+
+    console.log(coordinates);
+    marker = new mapboxgl.Marker({
     color: 'red',
     draggable: true
-}).setLngLat(coords)
+}).setLngLat(coordinates)
 .addTo(map)
-.on('dragend', event => console.log(event.target._lngLat))
+.on('dragend', event => console.log(event.target._lngLat));
+
+popup 
+    .setLngLat(coordinates)
+    .setHTML(
+        '<div><button onclick="window.location.replace(`/new/?street=${street}&plz=${plz}&city=${city}`)">Add a Location here 📍</button></div>'
+    )
+    .addTo(map);
+console.log(coordinates);
+const lat = coordinates.lat;
+const long = coordinates.lng;    
+
+// add geocoder
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+    })
+);
+
+mapboxClient.geocoding
+.reverseGeocode({
+    query: [long,lat]
+})
+.send()
+.then(function(response) {
+    if (
+        response &&
+        response.body &&
+        response.body.features &&
+        response.body.features.length
+      ) {
+        const fullAddress = response.body.features[0].place_name;
+        const splitAddress = fullAddress.split(',');
+        const plzCity = splitAddress[1].split(' ');
+        street = splitAddress[0];
+        plz = plzCity[1];
+        city = plzCity[2];
+        console.log(street);
+    }    
+});
+
+    // remove a marker
+    marker.getElement().addEventListener('clcik', () => {
+        marker.remove();
+    });
 };
 
+map.on('click', addMarker);
 
+axios.get('/location/')
+  .then(async res => {
+    //console.log(res)
+    const spaetis = res.data
+    console.log(res.data);
+    for (let i = 0; i < spaetis.length; i++) {
+      const coord = [locations[i].latitude, locations[i].longitude]
+      await addMarker2(coord)
+     }
+  })
 
+  function addMarker2 (coord) {
+   console.log(coord);
+    new mapboxgl.Marker({
+        color: "red",
+      })
+        .setLngLat(coord)
+        .addTo(map) 
+}
